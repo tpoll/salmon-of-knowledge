b4c18e9c994f9250304fd81281c4a0504afcdb37
Todd Pollak <todd.pollak@tufts.edu>
2015-12-01 20:22:50 -0500

    Creation and selection of small dataset

---
+++ parse_data.py
@@ -0,0 +1,20 @@
+import json
+import sys
+
+
+def main():
+    data = []
+    new = open("reviews.json", "wb")
+    with open(sys.argv[3], 'rb') as f:
+        for x in xrange(int(sys.argv[1]),int(sys.argv[2])):
+            review  = json.loads(f.readline())
+            data.append({'text': review["text"], 'stars': review["stars"]})
+
+
+    json.dump(data, new)
+    print data
+    new.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ yelp_data.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+
+reviews = {}
+
+def main():
+    global reviews
+
+    with open("reviews.json", 'rb') as f:
+        reviews = json.load(f)
+
+if __name__ == '__main__':
+    main()
+    for x in reviews:
+        print x
