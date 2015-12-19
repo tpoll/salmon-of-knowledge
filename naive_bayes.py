7ac8de22265b8394c8bd4b2e4441a97449b9ca38
Teddy Cleveland <tcclevela@gmail.com>
2015-12-19 16:35:42 -0500

    ashamed by failed attempts to implement sli

--- naive_bayes.py
+++ naive_bayes.py
@@ -8,76 +8,70 @@ import json
 class NaiveBayes(object):
     """NaiveBayes for sentiment analysis"""
     def __init__(self, vocab, stopwords):
-        self.positiveCounts = defaultdict(lambda: defaultdict(lambda: 1))
-        self.negativeCounts = defaultdict(lambda: defaultdict(lambda: 1))
         self.stopwords = stopwords
         self.positive = ImmutableSet([4, 5])
         self.negative = ImmutableSet([1, 2, 3])
         self.vocab = vocab
-
     
-    def Train(self, training_set, nGram=1):
+    def Train(self, training_set, N=1):
 
-        self.negativeNgrams = defaultdict(lambda: 1 + yelp_data.posVocabLen(self.vocab, training_set))
-        self.positiveNgrams = defaultdict(lambda: 1 + yelp_data.negVocabLen(self.vocab, training_set))
+        self.totalPositiveWords = len(self.vocab)
+        self.totalNegativeWords = len(self.vocab)
+        self.positiveCounts = defaultdict(lambda: defaultdict(lambda: 1))
+        self.negativeCounts = defaultdict(lambda: defaultdict(lambda: 1))
 
-        for N in range(1, nGram + 1):
+        for review in training_set:
+            if review["stars"] in self.positive:
+                self.totalPositiveWords += len(review["text"]) - N * 2
+            elif review["stars"] in self.negative:
+                self.totalNegativeWords += len(review["text"]) - N * 2
 
-            # get positive and negative counts
-            # for each word using review ratings.
-            for review in training_set:
-                if review['stars'] in self.positive:
-                    for i, word in enumerate(review['text'][nGram - N:]):
-                        if word is not "</S>":
-                            gram = tuple(review['text'][i - N:i])
-                            self.positiveNgrams[N] += 1
-                            self.positiveCounts[N][gram] += 1
-                else:
-                    for i, word in enumerate(review['text'][nGram - N:]):
-                        if word is not "</S>":
-                            gram = tuple(review['text'][i - N:i])
-                            self.negativeNgrams[N] += 1
-                            self.negativeCounts[N][gram] += 1
+            for i, word in enumerate(review["text"]):
+                for n in range(N, 0, -1):
+                    if i + n < len(review["text"]):
+                        gram = tuple(review["text"][i+N-n:i+N])
+                        if review["stars"] in self.positive:
+                            self.positiveCounts[n][gram] += 1
+                        elif review["stars"] in self.negative:
+                            self.negativeCounts[n][gram] += 1
+
 
     def __stupidBackoff(self, ngram, n, positive):
         alpha = 0.4
         if positive:
             gramCounts = self.positiveCounts
-            total = self.positiveNgrams
+            total = self.totalPositiveWords
         else:
             gramCounts = self.negativeCounts
-            total = self.negativeNgrams
+            total = self.totalNegativeWords
         if n > 1:
             if gramCounts[n][ngram] > 8:
                 return log(float(gramCounts[n][ngram]) / float(gramCounts[n-1][ngram[0:n-1]]))
             else:
-                return log(alpha) + self.__fuckingStupidBackoff(ngram[0:n-1], n-1, positive)
+                return log(alpha) + self.__stupidBackoff(ngram[0:n-1], n-1, positive)
         else:
-            return log(float(gramCounts[n][ngram]) / float(total[n]))
+            return log(float(gramCounts[n][ngram]) / float(total))
     
 
-    def PredictPositive(self, review, maxN):
+    def PredictPositiveStupidBackoff(self, review, maxN):
         p_positive = 0.0
         p_negative = 0.0
-        
-        for i, word in enumerate(review['text'][maxN - 1:]):
-            gram = tuple(review['text'][i-maxN:i])
-            for n in range(maxN, 1, -1):
-                top = gram[0:n]
-                p_positive += self.__fuckingStupidBackoff(top, n, True)
-                p_negative += self.__fuckingStupidBackoff(top, n, False)
 
-        if p_positive > p_negative:
-            return True
-        else:
-            return False
+        for i, word in enumerate(review['text']):
+            if i + maxN < len(review["text"]):
+                gram = tuple(review['text'][i:i+maxN])
+                p_positive += self.__stupidBackoff(gram, maxN, True)
+                p_negative += self.__stupidBackoff(gram, maxN, False)
+
+        return p_positive > p_negative
+
 
 def main():
 
-    maxN = 3
+    maxN = 4
     reviews = yelp_data.getReviews()
-    training_set = reviews[0:5000]
-    test_set     = reviews[5001:10001]
+    training_set = reviews[0:9000]
+    test_set     = reviews[9001:18000]
     vocab = yelp_data.buildVocab(training_set)
     training_set_prep = yelp_data.preProcessN(training_set, vocab, maxN)
     test_set_prep = yelp_data.preProcessN(test_set, vocab, maxN)
@@ -101,14 +95,15 @@ def main():
     print "Running test data on constructed model...."
     print "----------------------------------"
     
-    #Test accuracy
+
+    # Test accuracy
     total = 0.0
     right = 0.0
     for review in test_set_prep:
         total += 1.0
-        if review['stars'] in naiveBayes.positive and naiveBayes.PredictPositive(review, maxN):
+        if review['stars'] in naiveBayes.positive and naiveBayes.PredictPositiveStupidBackoff(review, maxN):
             right += 1.0
-        elif review['stars'] in naiveBayes.negative and not naiveBayes.PredictPositive(review, maxN):
+        elif review['stars'] in naiveBayes.negative and not naiveBayes.PredictPositiveStupidBackoff(review, maxN):
             right += 1.0
 
     print "Percent Accuracy using Stupid Backoff and Laplace Smoothing:", ((right/total) * 100), "\n"
