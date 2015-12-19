c81a12fd56ceadeff6e950536db4d1b637b117f3
Todd Pollak <todd.pollak@tufts.edu>
2015-12-19 13:21:49 -0500

    N-grams for maxent

--- normalize.sh
+++
@@ -1,6 +0,0 @@
- java -cp $WEKA_INS/weka.jar weka.filters.unsupervised.attribute.Standardize \
-   -b \
-   -i yelp_maxent_training.arff \
-   -o train_std.arff \
-   -r yelp_maxent_test.arff \
-   -s test_std.arff
